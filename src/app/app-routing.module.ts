43f4a19e5b0ba0bab10f2265d2beb748de6fa1f0
deepthiTalapagala <116480012+deepthiTalapagala@users.noreply.github.com>
2024-04-07 15:07:43 +0530

    Added:Experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EducationComponent } from './education/education.component';
 
-const routes: Routes = [
-  {path: "education", component:EducationComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
